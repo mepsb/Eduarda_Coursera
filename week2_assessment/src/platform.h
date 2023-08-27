76f75464246302c62e7eabad269f0bb6496387d7
mepsb <1160785@isep.ipp.pt>
2023-08-28 00:36:20 +0100

    Functional-ish version of Makefile

---
+++ week2_assessment/src/platform.h
@@ -0,0 +1,37 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+#ifndef __PLATFORM_H__
+#define __PLATFORM_H__
+
+/******************************************************************************
+ Platform - MSP432
+******************************************************************************/
+#if defined (MSP432)
+#   pragma message "Platform MSP432 is defined"
+#include "msp432p401r.h"
+#define PRINTF(...)
+/******************************************************************************
+ Platform - HOST
+******************************************************************************/
+#elif defined (HOST)
+#   pragma message "Platform HOST is defined"
+#include <stdio.h>
+#define PRINTF(...) printf(__VA_ARGS__)
+/******************************************************************************
+ Platform - Unsupported
+******************************************************************************/
+#else
+#define printf(PLATFORM)
+#error "Platform provided is not supported in this Build System"
+#endif
+
+#endif /* __PLATFORM_H__ */
+
