eb4057b053e1fd2962911731eb647b9dd78fc336
mepsb <1160785@isep.ipp.pt>
2023-08-27 22:26:55 +0100

    Makefile and sources.mk initial implementation

--- week2_assessment/src/sources.mk
+++ week2_assessment/src/sources.mk
@@ -10,7 +10,7 @@
 #*****************************************************************************
 
 # Add your Source files to this variable
-SOURCES =
+SOURCES = main.c 
 
 # Add your include paths to this variable
 INCLUDES = 
